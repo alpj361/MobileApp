3d69a790c251acf713ab122af1c7d70fb6a4045e
pj <alpj361@gmail.com>
2025-11-04 02:27:36 -0600

    fix: require NativeWind conditionally to avoid loading lightningcss on Netlify

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,5 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
@@ -27,5 +26,7 @@ if (shouldDisableNativeWind) {
   module.exports = config;
 } else {
   console.log('[Metro] NativeWind enabled (local/native build)');
+  // Only require NativeWind when we actually need it
+  const { withNativeWind } = require('nativewind/metro');
   module.exports = withNativeWind(config, { input: './global.css' });
 }
